b501895d5bc43eae266defc6f6006cf66a6cd21c
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-05-17 01:17:37 -0500

    feat: :sparkles: web component of all clothing, t-shirts and pants
    The web components of all the clothes, t-shirts and pants were created and they were all placed under the same CSS.

--- js/components/ropa.js
+++ js/components/ropa.js
@@ -1,3 +1,45 @@
+export class todos extends HTMLElement{
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"});
+    }
+    connectedCallback(){
+        this.ropa_toda();
+    }
+    async ropa_toda(){
+        const bdata = await fetch("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
+        console.log(bdata)
+        const data = await bdata.json();
+        console.log(data)
+        this.renderData(data);
+    }
+        renderData(data){
+            const style = `
+            <link rel="stylesheet" href="../css/ropa.css">
+            <script src="./js/main.js"></script>`;
+            let content = `${style}`;
+
+            data.forEach(val =>{
+                content += `
+                <div class="items">
+                    <img src="${val.img}" alt="ropa">
+                    <div class="container">
+                        <p>${val.description}</p>
+                        <p>${val.value}</p>
+                        <div class="btn">
+                            <button onclick="incrementar('contador')">Agregar</button> 
+                        </div>
+                    </div>
+                </div>
+            `
+            })
+        this.shadowRoot.innerHTML = content;
+        }
+        
+}
+customElements.define("toda-ropa" , todos)
+
+
 export class abrigos extends HTMLElement{
     constructor(){
         super()
@@ -40,3 +82,91 @@ export class abrigos extends HTMLElement{
         
 }
 customElements.define("my-abrigos" , abrigos)
+
+
+export class camisetas extends HTMLElement{
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"});
+    }
+    connectedCallback(){
+        this.ropa_camisetas();
+    }
+    async ropa_camisetas(){
+        const bdata = await fetch("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
+        console.log(bdata)
+        const data = await bdata.json();
+        console.log(data)
+        this.renderData(data);
+    }
+        renderData(data){
+            const style = `
+            <link rel="stylesheet" href="../css/ropa.css">
+            <script src="./js/main.js"></script>`;
+            let content = `${style}`;
+
+            data.forEach(val =>{
+                if (val.id == "camiseta"){
+                    content += `
+                    <div class="items">
+                        <img src="${val.img}" alt="ropa">
+                        <div class="container">
+                            <p>${val.description}</p>
+                            <p>${val.value}</p>
+                            <div class="btn">
+                                <button onclick="incrementar('contador')">Agregar</button> 
+                            </div>
+                        </div>
+                    </div>
+                `
+                }
+            })
+        this.shadowRoot.innerHTML = content;
+        }
+        
+}
+customElements.define("my-camisetas" , camisetas)
+
+
+export class pantalones extends HTMLElement{
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"});
+    }
+    connectedCallback(){
+        this.ropa_pantalones();
+    }
+    async ropa_pantalones(){
+        const bdata = await fetch("https://6608363ca2a5dd477b14291c.mockapi.io/allProduct")
+        console.log(bdata)
+        const data = await bdata.json();
+        console.log(data)
+        this.renderData(data);
+    }
+        renderData(data){
+            const style = `
+            <link rel="stylesheet" href="../css/ropa.css">
+            <script src="./js/main.js"></script>`;
+            let content = `${style}`;
+
+            data.forEach(val =>{
+                if (val.id == "pantalon"){
+                    content += `
+                    <div class="items">
+                        <img src="${val.img}" alt="ropa">
+                        <div class="container">
+                            <p>${val.description}</p>
+                            <p>${val.value}</p>
+                            <div class="btn">
+                                <button onclick="incrementar('contador')">Agregar</button> 
+                            </div>
+                        </div>
+                    </div>
+                `
+                }
+            })
+        this.shadowRoot.innerHTML = content;
+        }
+        
+}
+customElements.define("my-pantalones" , pantalones)
